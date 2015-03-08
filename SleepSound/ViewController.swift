a55fe00db5124e3d42d69682676b23fa7ceadd9c
Tom Seward <sewardth@gmail.com>
2015-03-07 23:39:20 -0500

    Initial UI developed

--- SleepSound/ViewController.swift
+++ SleepSound/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    var pickTime = NSDate()
+    
+   
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        playButton.hidden = false
+        stopButton.hidden = true
+        if  var filePath = NSBundle.mainBundle().pathForResource("red_noise", ofType: "wav")
+        {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+        }
+            
+        else
+        {
+            println("the file path is empty")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +42,39 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func playSound(sender: UIButton) {
+        playButton.hidden = true
+        stopButton.hidden = false
+        var currentTimeParts = getDateComponents(NSDate())
+        var pickTimeParts = getDateComponents(pickTime)
+        while (currentTimeParts["hour"] != pickTimeParts["hour"]) && (currentTimeParts["minutes"] != pickTimeParts["minutes"]){
+            println("playing sound")
+            audioPlayer.play()
+            currentTimeParts = getDateComponents(NSDate())
+        }
+        println("restraints match")
+        
+        
+        
+        
+    }
+
+    @IBAction func getDate(sender: UIDatePicker) {
+        pickTime = sender.date
+    }
+    
+    @IBAction func stopSound(sender: UIButton) {
+        playButton.hidden = false
+        stopButton.hidden = true
+        audioPlayer.stop()
+    }
+    
+    func getDateComponents (date:NSDate)->Dictionary<String, Int>!{
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate:  date)
+        let timeParts = ["hour":components.hour, "minutes":components.minute]
+        return timeParts
+        
+    }
 }
 
