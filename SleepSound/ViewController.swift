9a920122af044097cd35e4aadeab2ad0cc09f3a4
Tom Seward <sewardth@gmail.com>
2015-03-07 19:45:26 -0500

    Initial Commit

---
+++ SleepSound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepSound
+//
+//  Created by Tom Seward on 3/7/15.
+//  Copyright (c) 2015 Tom Seward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
